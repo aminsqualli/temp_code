f761820c4f8d54ebc7d95dafb7188cb80244cb2f
aminsqualli <73706281+aminsqualli@users.noreply.github.com>
2024-01-09 15:32:20 -0500

    Update db.py

--- db.py
+++ db.py
@@ -19,8 +19,16 @@ table2 = '3Hshsh_theta'
 # Create the view with column aliasing
 view_query = f'''
 CREATE VIEW combined_view AS
-SELECT t1.*, t2.*,
-       t1."Date" AS combined_date
+SELECT
+    t1.column1 AS column1_{table1},
+    t1.column2 AS column2_{table1},
+    -- Repeat for all columns from table1
+    t1."Date" AS Date_{table1},
+    
+    t2.column1 AS column1_{table2},
+    t2.column2 AS column2_{table2},
+    -- Repeat for all columns from table2
+    t2."Date" AS Date_{table2}
 FROM "{table1}" t1
 FULL OUTER JOIN "{table2}" t2 ON t1."Date" = t2."Date"
 ORDER BY COALESCE(t1."Date", t2."Date");
