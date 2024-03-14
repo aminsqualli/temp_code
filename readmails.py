174dfc37ffdec91234d375276c195df78f175bfc
aminsqualli <73706281+aminsqualli@users.noreply.github.com>
2024-03-14 11:25:49 -0400

    Update readmails.py

--- readmails.py
+++ readmails.py
@@ -1,5 +1,6 @@
 import win32com.client
 import datetime
+import pytz
 
 # Connect to Outlook
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
@@ -8,11 +9,22 @@ inbox = outlook.GetDefaultFolder(6)  # "6" refers to the Inbox folder
 # Get today's date
 today = datetime.datetime.today()
 
-# Process emails received today
-for email in inbox.Items:
-    if email.Class == 43:  # Class 43 refers to a mail item
-        if email.ReceivedTime.date() == today.date():
-            print("Subject:", email.Subject)
-            print("Sender:", email.SenderName)
-            print("Received Time:", email.ReceivedTime)
-            print("-------------------")
+# Set the time zone to the local time zone
+local_timezone = pytz.timezone('YOUR_TIMEZONE')  # Replace 'YOUR_TIMEZONE' with your local time zone
+today_local = datetime.datetime.now(local_timezone)
+
+# Get the start of the day in the local time zone
+start_of_day_local = today_local.replace(hour=0, minute=0, second=0, microsecond=0)
+
+# Format the date string with the adjusted start of the day
+date_string = start_of_day_local.strftime('%m/%d/%Y %H:%M:%S')
+
+# Get emails received today
+received_today = inbox.Items.Restrict("[ReceivedTime] >= '" + date_string + "'")
+
+# Print subject and sender of each email received today
+for email in received_today:
+    print("Subject:", email.Subject)
+    print("Sender:", email.SenderName)
+    print("Received Time:", email.ReceivedTime)
+    print("-------------------")
